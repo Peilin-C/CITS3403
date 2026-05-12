65ebb26ba88a582bb32953736a64fac180b86f92
Bhavya Narula <bhavyanarula66@gmail.com>
2026-05-12 16:29:16 +0800

    Add CSRF protection to all forms — closes #90

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'danger'
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
@@ -14,6 +16,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    csrf.init_app(app)
 
     from app.routes import main
     from app.auth import auth
